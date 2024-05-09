c13754a4a078e8fc8d1f013efe953ebee3b85fc2
Eric <eschuck58@gmail.com>
2024-05-09 16:14:04 +0200

    fix test 2

--- scripts/script.js
+++ scripts/script.js
@@ -52,7 +52,7 @@ if (width <= 1000) {
   document.querySelector(".indexHTML").innerHTML = `
   <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="user-scalable=0"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Philosopher:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
